300ded9f62e9763dd8fc44faca750607fb5adf60
= <ngaboiris77@gmail.com>
2023-12-18 11:36:52 +0200

    updated end to end testing

--- exam/exam/src/test/java/termone/devops/exam/MathCalculationEndToEnd.java
+++ exam/exam/src/test/java/termone/devops/exam/MathCalculationEndToEnd.java
@@ -1,8 +1,12 @@
 package termone.devops.exam;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,13 +15,22 @@ public class MathCalculationEndToEnd {
     @LocalServerPort
     private int port;
 
-    private final TestRestTemplate restTemplate = new TestRestTemplate();
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void testApiEndpoint() {
-        String url = "http://localhost:" + port + "/api/someEndpoint";
-        String response = restTemplate.getForObject(url, String.class);
+        // Given
+        String url = "http://localhost:" + port + "/api/v1/do_math";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String requestBody = "{\"operand1\": 2, \"operand2\": 5, \"operation\": \"+\"}";
 
+        // When
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+        String response = restTemplate.postForObject(url, request, String.class);
+
+        // Then
         assertEquals("Expected Response", response);
     }
 }
